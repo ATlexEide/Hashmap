5b0937e25e8a590f1ab844373fc325b2b85bbfaf
Alexander Eide <atlexeide@gmail.com>
2024-10-13 20:24:04 +0200

    Add has / remove / length logic

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ class HashMap {
     this.size = size;
     this.loadFactor = loadFactor;
     this.currentSize = 0;
-    this.buckets = [];
+    this.buckets = {};
+    this.noKeyError = "Error: No Key (Key does not exist)";
   }
   hash(key) {
     let hashCode = 0;
@@ -14,19 +15,31 @@ class HashMap {
     return hashCode;
   }
   set(key, value) {
-    this.buckets.push([this.hash(key), value]);
+    key = this.hash(key);
+    console.log("in SET: " + key);
+    this.buckets[key] = value;
     this.currentSize++;
   }
   get(key) {
-    console.log(this.hash(key));
-    for (const bucket of this.buckets) {
-      if (bucket[0] === this.hash(key)) return bucket[1];
-    }
-    return "Error: Key not valid (no match)";
+    key = this.hash(key);
+    if (key in this.buckets) return this.buckets[key];
+    else return this.noKeyError;
   }
-  has(key) {}
-  remove(key) {}
-  length() {}
+  has(key) {
+    key = this.hash(key);
+    if (key in this.buckets) return true;
+    else return false;
+  }
+  remove(key) {
+    key = this.hash(key);
+    if (key in this.buckets) {
+      delete this.buckets[key];
+      this.currentSize--;
+    } else console.log(this.noKeyError);
+  }
+  get length() {
+    return this.currentSize;
+  }
   clear() {}
   keys() {}
   values() {}
@@ -36,7 +49,12 @@ class HashMap {
 const testMap = new HashMap(16);
 
 testMap.set("test", "valuableValue");
-testMap.set("test2", "valuableValue2");
-console.log(testMap.currentSize);
-console.log(testMap.buckets[0]);
-console.log(testMap.get("test22"));
+//////////
+console.log("BEFORE");
+console.log(testMap.buckets);
+///////
+testMap.remove("test");
+///////
+console.log("AFTER");
+console.log(testMap.buckets);
+console.log(testMap.length);
