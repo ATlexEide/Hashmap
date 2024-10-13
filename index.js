477e564a88d3d720e050a59076c224738ac9fada
Alexander Eide <atlexeide@gmail.com>
2024-10-13 17:39:30 +0200

    Add base set && get logic

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-class HashMap{
-    constructor(size, loadFactor = 0.75){
-        this.size = size;
-        this.loadFactor = loadFactor;
+class HashMap {
+  constructor(size, loadFactor = 0.75) {
+    this.size = size;
+    this.loadFactor = loadFactor;
+    this.currentSize = 0;
+    this.buckets = [];
+  }
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
-    hashCode(key){
-        let hashCode = 0;
-        const primeNumber = 31;
-        for(let i = 0; i < key.length ; i++){
-            console.log(i)
-                hashCode += (primeNumber * key.charCodeAt(i)) % this.size
-        }
-        return hashCode;
-    };
-    set(key, value){
-
-    };
-    get(key){
-
-    };
-    has(key){
-
-    };
-    remove(key){
-
-    };
-    length(){
-
-    };
-    clear(){
-
-    };
-    keys(){
-
-    };
-    values(){
-
-    };
-    entries(){
-
-    };
+    return hashCode;
+  }
+  set(key, value) {
+    this.buckets.push([this.hash(key), value]);
+    this.currentSize++;
+  }
+  get(key) {
+    console.log(this.hash(key));
+    for (const bucket of this.buckets) {
+      if (bucket[0] === this.hash(key)) return bucket[1];
+    }
+    return "Error: Key not valid (no match)";
+  }
+  has(key) {}
+  remove(key) {}
+  length() {}
+  clear() {}
+  keys() {}
+  values() {}
+  entries() {}
 }
 
 const testMap = new HashMap(16);
 
-
-console.log("Mod: "+testMap.hashCode("test"))
+testMap.set("test", "valuableValue");
+testMap.set("test2", "valuableValue2");
+console.log(testMap.currentSize);
+console.log(testMap.buckets[0]);
+console.log(testMap.get("test22"));
